80aadd7e245c03885d91001788193a8b94202157
Domenico Guarino <domenico.guarino@gmail.com>
2019-09-12 15:27:18 +0200

    added new stimulus runnning scripts

---
+++ run_annulus_ffw.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+This is 
+"""
+from pyNN import nest
+import sys
+import mozaik
+import mozaik.controller
+from mozaik.controller import run_workflow, setup_logging
+from mozaik.storage.datastore import Hdf5DataStore, PickledDataStore
+from parameters import ParameterSet
+
+from model_feedforward import ThalamoCorticalModel
+    
+from experiments import create_experiments_annulus
+
+
+try:
+    from mpi4py import MPI
+except ImportError:
+    MPI = None
+if MPI:
+    mpi_comm = MPI.COMM_WORLD
+MPI_ROOT = 0
+
+logger = mozaik.getMozaikLogger()
+
+
+# Manage what is executed
+# a set of variable here to manage the type of experiment and whether the pgn, cortex are there or not.
+withPGN = True  # 
+withV1 = True  # open-loop
+withFeedback_CxPGN = False # ffw loop
+withFeedback_CxLGN = False # ffw loop
+
+withRandomV1conns = False
+
+# Model execution
+data_store,model = run_workflow('ThalamoCorticalModel', ThalamoCorticalModel, create_experiments_annulus )
+data_store.save()
+
---
+++ run_bar_open.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+This is 
+"""
+from pyNN import nest
+import sys
+import mozaik
+import mozaik.controller
+from mozaik.controller import run_workflow, setup_logging
+from mozaik.storage.datastore import Hdf5DataStore, PickledDataStore
+from parameters import ParameterSet
+
+from model import ThalamoCorticalModel
+    
+from experiments import create_experiments_brokenbar
+
+from analysis_and_visualization import perform_analysis_and_visualization
+
+
+try:
+    from mpi4py import MPI
+except ImportError:
+    MPI = None
+if MPI:
+    mpi_comm = MPI.COMM_WORLD
+MPI_ROOT = 0
+
+logger = mozaik.getMozaikLogger()
+
+
+withPGN = True
+withV1 = True
+withFeedback_CxPGN = False # closed loop
+withFeedback_CxLGN = False # closed loop
+
+# Model execution
+if True:
+    data_store,model = run_workflow('ThalamoCorticalModel', ThalamoCorticalModel, create_experiments_brokenbar )
+    data_store.save()
+# or only load pickled data
+else:
+    setup_logging()
+    data_store = PickledDataStore(load=True,parameters=ParameterSet({'root_directory':'ThalamoCorticalModel_data_brokenbar_closed_____', 'store_stimuli' : False}),replace=True)
+    logger.info('Loaded data store')
+
+    # # Analysis and Plotting
+    # if mpi_comm.rank == MPI_ROOT:
+    #     perform_analysis_and_visualization( data_store, 'spatial_frequency', withPGN, withV1 )
+
+    # data_store.save()
